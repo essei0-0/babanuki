2f5f01b7e1e8de56279d5214692e8d673964ea34
essei <hello.essei@gmail.com>
2021-10-19 15:11:48 +0900

    Game.prepare 関数切り出し

--- model/game.rb
+++ model/game.rb
@@ -23,97 +23,48 @@ class Game
     end
 
     def prepare()
-        # 人数を決める(とりあえず4人で)
-        player = Player.new("P1", false)
+        create_players
+        display_players
+        distribute_cards
+        display_my_cards
 
-        cp_names = ['com1', 'com2', 'com3']
-        cps = Player.create_cps(cp_names)
-
-        @players = [player] + cps
-        
-        puts ""
-        #sleep(1)
-        puts "##############################"
-        puts "対戦相手はこちらです。"
-        @players.each do |player|
-            player.is_cp ? (puts "cp    ：#{player.name}") : (puts "あなた：#{player.name}")
-        end
-        puts "##############################"
-
-        #トランプカードを生成する。
-        cards = Trump.new()
-
-        #トランプカードをシャッフルする。
-        shuffled_cards = cards.shuffled()
-        # p shuffled_cards
-
-        #トランプカードを配る
-        puts ""
-        puts "カードを配ってます。"
-        shuffled_cards.each_with_index do |card, i|
-            @players[i%4].get_card(card)
-        end
-        #sleep(1)
-        #手札確認
-        puts "カードを配りました。"
-        #sleep(1)
-        puts "あなた(#{@players[0].name})の手札はこちらです。"
-        #sleep(1)
-        puts ""
-        puts @players[0].check_cards
-        
-        # デバッグ用
-        # puts @players[1].name, @players[1].check_cards
-        # puts "--------------------"
-        # puts @players[2].name, @players[2].check_cards
-        # puts "--------------------"
-        # puts @players[3].name, @players[3].check_cards
-
-        #順番を決める
         puts ""
         puts "順番を決めています。"
+        puts ""
 
-        @shuffled_players = @players.shuffle
-        @shuffled_players.each_with_index do |player, i|
-            @shuffled_players[i].target = @shuffled_players[(i+1)%4]
-            # puts("#{@shuffled_players[i].name} : #{@shuffled_players[i].target.name}")
-        end
-        #sleep(1)
+        decide_play_order
+
+        puts "順番が決まりました。"
+        puts ""
+
+        display_play_order
+
+        sleep(1)
     end
 
     def start()
-        #手持ちのペアカードを捨てる処理
         puts ""
         puts "ペアのカードを捨てています。"
-        @shuffled_players.each do |player|
-            player.cards.each_with_index do |card, i|     
-                next if card.place == 'table'
-                card_num_array = player.cards[(i+1)..-1].map(&:number)
-                if pear_index = card_num_array.find_index(card.number)
-                    card.place = 'table'
-                    player.cards[i + pear_index].place = 'table'
-                end
-            end
-        end
+        discard_pair_cards
         #sleep(1)
 
+        #ターン数
         turn = 1
+        #残りプレイヤー
         remaining_player = 4
+
+        #各プレイヤーのランキングを初期化
         @shuffled_players.map{|player| player.rank = 4}
 
-        ready = 'n'
-        while ready != 'y'
-            puts ""
-            puts "ゲームを始めます。準備はいいですか？(y/n)"
-            ready = $stdin.gets.chomp!
-        end
+        confirm_before_start
 
         puts ""
         puts "##############################"
         puts "Game Start!"
         puts "##############################"
-        #sleep(1)
+        sleep(1)
 
+        
         while remaining_player > 1 do
             puts ""
             puts "##############################"
@@ -230,4 +181,89 @@ class Game
     end
 
     private
+    def create_players
+        # 人数を決める(とりあえず4人で)
+        player = Player.new("P1", false)
+
+        cp_names = ['com1', 'com2', 'com3']
+        cps = Player.create_cps(cp_names)
+
+        @players = [player] + cps
+    end
+
+    def display_players
+        puts ""
+        puts "##############################"
+        puts "対戦相手はこちらです。"
+        @players.each do |player|
+            player.is_cp ? (puts "cp    ：#{player.name}") : (puts "あなた：#{player.name}")
+        end
+        puts "##############################"
+    end
+
+    def distribute_cards
+        #トランプカードを生成する。
+        cards = Trump.new()
+
+        #トランプカードをシャッフルする。
+        shuffled_cards = cards.shuffled()
+        # p shuffled_cards
+
+        #トランプカードを配る
+        puts ""
+        puts "カードを配っています。"
+        shuffled_cards.each_with_index do |card, i|
+            @players[i%4].get_card(card)
+        end
+        puts "カードを配りました。"
+    end
+
+    def display_my_cards
+        #自分の手札を表示する。
+        puts "あなた(#{@players[0].name})の手札はこちらです。"
+        puts ""
+        puts @players[0].check_cards
+    end
+
+    def decide_play_order
+        #順番を決める
+        @shuffled_players = @players.shuffle
+        @shuffled_players.each_with_index do |player, i|
+            @shuffled_players[i].target = @shuffled_players[(i+1)%4]
+        end
+    end
+
+    def display_play_order
+        #プレイ順を表示する
+        @shuffled_players.each_with_index do |player, i|
+            puts "#{i+1}番目：#{player.name}"
+        end
+    end
+
+    def discard_pair_cards
+        #配られた手持ちのペアカードを捨てる処理
+        @shuffled_players.each do |player|
+            player.cards.each_with_index do |card, i|     
+                next if card.place == 'table'
+                card_num_array = player.cards[(i+1)..-1].map(&:number)
+                if pear_index = card_num_array.find_index(card.number)
+                    card.place = 'table'
+                    player.cards[i + pear_index].place = 'table'
+                end
+            end
+        end
+    end
+
+    def confirm_before_start
+        ready = 'n'
+        while ready != 'y'
+            puts ""
+            puts "ゲームを始めます。準備はいいですか？(y/n)"
+            ready = $stdin.gets.chomp!
+        end
+    end
+
+
+
+
 end
